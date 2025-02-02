0f3410f9e3db3cae809e308aa809c5c577727bb1
javierfcordobae <jcord15@bancodebogota.com.co>
2025-02-02 15:56:51 -0500

    add cors config

---
+++ mail-manager/src/main/java/com/bd/edu/co/mail_manager/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.bd.edu.co.mail_manager.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Apply to specific paths
+                        .allowedOrigins("http://example.com") // Allow only this origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
+}

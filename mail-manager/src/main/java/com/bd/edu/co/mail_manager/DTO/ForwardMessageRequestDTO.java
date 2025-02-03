4929f4c2902332875d80ec877ebd753e9da31bc7
javierfcordobae <jcord15@bancodebogota.com.co>
2025-02-03 14:30:10 -0500

    add corsconfig to SecurityConfig

--- mail-manager/src/main/java/com/bd/edu/co/mail_manager/DTO/ForwardMessageRequestDTO.java
+++ mail-manager/src/main/java/com/bd/edu/co/mail_manager/DTO/ForwardMessageRequestDTO.java
@@ -9,4 +9,52 @@ public class ForwardMessageRequestDTO {
     String idType;
     String username;
     String newBody;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getSenderUsername() {
+        return senderUsername;
+    }
+
+    public void setSenderUsername(String senderUsername) {
+        this.senderUsername = senderUsername;
+    }
+
+    public List<String> getMailContactos() {
+        return mailContactos;
+    }
+
+    public void setMailContactos(List<String> mailContactos) {
+        this.mailContactos = mailContactos;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNewBody() {
+        return newBody;
+    }
+
+    public void setNewBody(String newBody) {
+        this.newBody = newBody;
+    }
 }
